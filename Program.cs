a2dea05a3394e0b5082c1a0540c928bfa613355e
mutandre <61994669+mutandre@users.noreply.github.com>
2020-03-23 20:16:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 4;
+            Console.WriteLine("Raiz quadrada do valor inserido é "+ Math.Sqrt (x));
+            Console.WriteLine ("Exponecial do valaor "+ Math.Pow(Math.Sqrt(x),3));
+        }
+    }
+}
