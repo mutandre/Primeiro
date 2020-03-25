6cf1a76ced74bf6b884880b5632c58a7da08e003
Mutu Andre <mutuandre@gmail.com>
2020-03-25 20:25:57 -0300

    Atualizacao

--- Program.cs
+++ Program.cs
@@ -8,12 +8,16 @@ namespace _01
         {
            Double x = 0;
            Double y = 0;
+            char cartao;
 
         Console.WriteLine ("Digite o valor do primeiro  número: " );
         x = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine ("Digite o valor do primeiro  número: " );
         y = Convert.ToDouble(Console.ReadLine());
-        
+        cartao = Convert.ToChar(Console.ReadLine());
+
+         if (cartao == 'S' || cartao == 's')
+
         Console.WriteLine("Raiz quadrada do valor inserido é "+ (x+y));
         Console.ReadLine();
         }
